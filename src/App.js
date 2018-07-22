b25ae569b575690f92cdb6c5fe3886808f320f17
koshhi <cesaralvarezmarin@gmail.com>
2018-07-22 23:08:43 +0200

    added sass preprocessing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
+import './App.scss';
 
 class App extends Component {
   render() {
